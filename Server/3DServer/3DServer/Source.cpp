95aced60e4963cfd00fc2bddfbd81dc36728cbc5
Antonii <sergiureznicencu@gmail.com>
2021-02-27 14:48:27 +0200

    Cleaned up master

--- Server/3DServer/3DServer/Source.cpp
+++ Server/3DServer/3DServer/Source.cpp
@@ -149,20 +149,8 @@ void processConnections() {
 
 bool parser(CLIENT_STRUCTURE &client) {
 	// Http parser goes here - this is called when the socket has data on the buffer to be read
-
-	char* buff = new char[HTTP_HEADER_MAX_SIZE];
-	bool parser_result = false;
-
-	if (!(parser_result = recvAll(client, buff, 200))) {
-		closeSelectedClient(client);
-		delete[] buff;
-		return false;
-	}
-
-	buff[200 - 1] = 0;
-	printf("Received: %s", buff);
-
-	delete[] buff;
+	
+	return true;
 }
 
 // Block until all bytes are received
