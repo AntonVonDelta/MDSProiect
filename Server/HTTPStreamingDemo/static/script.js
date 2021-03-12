3c04fcf1c814970595ddd650ff96938ed93c471d
Vlad Pirlog <avi.pirlog@gmail.com>
2021-03-12 15:12:38 +0200

    Add progress bar for downloads

--- Server/HTTPStreamingDemo/static/script.js
+++ Server/HTTPStreamingDemo/static/script.js
@@ -5,6 +5,8 @@
     const xsmallImgBtn = document.getElementById('my-btn-xsmall')
     const smallImgBtn = document.getElementById('my-btn-small')
     const mediumImgBtn = document.getElementById('my-btn-medium')
+    const progressBar = document.getElementById('progress-bar')
+    const progressBarText = document.getElementById('progress-bar-text')
 
     /**
      * @type {HTMLCanvasElement}
@@ -18,10 +20,11 @@
 
         const width = parseInt(res.headers.get(IMAGE_WIDTH_HEADER)) || 720
         const height = parseInt(res.headers.get(IMAGE_HEIGHT_HEADER)) || 480
+        const size = width * height * 4
 
         const reader = res.body.getReader()
         let loaded = 0
-        const arr = new Uint8ClampedArray(width * height * 4)
+        const arr = new Uint8ClampedArray(size)
         while (true) {
             const { done, value } = await reader.read()
             if (done) break
@@ -29,13 +32,16 @@
                 arr[i + loaded] = value[i]
             }
             loaded += value.length
-            console.log(`${loaded} bytes downloaded...`)
+            const percentage = Math.round(100 * loaded / size)
+            progressBar.style.width = `${percentage}%`
+            progressBarText.textContent = `${percentage}% downloaded...`
+
             ctx.putImageData(new ImageData(arr, width, height), 0, 0)
         }
+        progressBarText.textContent = 'done'
     }
 
     mediumImgBtn.addEventListener('click', () => fetchAndHandleImageStream('/raw_medium'))
     smallImgBtn.addEventListener('click', () => fetchAndHandleImageStream('/raw_small'))
     xsmallImgBtn.addEventListener('click', () => fetchAndHandleImageStream('/raw_xsmall'))
-
 })()
